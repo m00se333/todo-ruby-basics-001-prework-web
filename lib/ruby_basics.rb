bb554710846660643bec6646f63967d90e34f2b9
m00se333 <moosecunningham@gmail.com>
2015-09-10 14:58:35 -0700

    Useful lab good review

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
 
-def assign_variable(value)
+def assign_variable(name)
+    person = name
+  end
+
+def argue(butts)
+    butts
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting(x, y)
 
 end
 
 def return_a_value
+    return "Nice"
 
 end
 
 def last_evaluated_value
+    puts "hello"
+    puts "butts"
+    return "expert"
 
 end
 
-def pizza_party
+def pizza_party(topping = "cheese")
+    return topping
 
 end
